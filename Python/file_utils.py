f448de64adff83b6a62ee9af0aa2bf574dbb6bc8
Boutin <boutin.l@stjolorient.fr>
2025-01-13 16:21:18 +0100

    Python

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -2,7 +2,9 @@
 
 def diff(file_first, file_second):
     """Fonction retournant True si deux fichiers sont différents."""
-    return True
+    with open(file_first) as file1_id :
+		with open(file_second) as file2_id :
+			return file1_id.read() != file2_id.read()
 
 def same(file_first, file_second):
     """Fonction retournant True si deux fichiers sont identiques."""
